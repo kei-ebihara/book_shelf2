049f82ec0df274e30ced7bfad24a1080f0324221
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2019-12-11 00:37:01 +0900

    書籍登録機能完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,15 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to @book, notice: "書籍を登録しました。"
+    if @book.save
+      redirect_to @book, notice: "書籍を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 
   private
