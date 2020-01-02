5e4dbc283c0798c3f2b2b364561d830886d6195d
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2020-01-02 21:09:12 +0900

    レビューの管理機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
-Rails.application.routes.draw do
-  resources :reviews
-  devise_for :users
-  resources :books
+  Rails.application.routes.draw do
+    devise_for :users
+    resources :books do
+      resources :reviews, except: :index
+    end
   root 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
